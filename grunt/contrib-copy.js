df9cee5b23e2d822095a2cf61bde9ffcbcf7d8cb
Michael Westwood <michael@du.st>
2016-06-28 10:39:59 +0100

    Now plugins are pulled in from git and copied over with grunt.

--- grunt/contrib-copy.js
+++ grunt/contrib-copy.js
@@ -9,6 +9,51 @@ module.exports = function(grunt) {
 				dest: '<%= dirs.dest %>/assets/fonts/',
 				flatten: false
 			}]
+		},
+		pluginanchor: {
+			files: [{
+				expand: true,
+				cwd: 'bower_components/craft-anchor/anchor/',
+				src: ['**'],
+				dest: 'craft/plugins/anchor/',
+				flatten: false
+			}]
+		},
+		plugincachey: {
+			files: [{
+				expand: true,
+				cwd: 'bower_components/craft-cachey/cachey/',
+				src: ['**'],
+				dest: 'craft/plugins/cachey/',
+				flatten: false
+			}]
+		},
+		pluginemailwrap: {
+			files: [{
+				expand: true,
+				cwd: 'bower_components/craft-emailwrap/emailwrap/',
+				src: ['**'],
+				dest: 'craft/plugins/emailwrap/',
+				flatten: false
+			}]
+		},
+		pluginrefreshstring: {
+			files: [{
+				expand: true,
+				cwd: 'bower_components/craft-refreshstring/refreshstring/',
+				src: ['**'],
+				dest: 'craft/plugins/refreshstring/',
+				flatten: false
+			}]
+		},
+		plugintypogrify: {
+			files: [{
+				expand: true,
+				cwd: 'bower_components/typogrify/typogrify/',
+				src: ['**'],
+				dest: 'craft/plugins/typogrify/',
+				flatten: false
+			}]
 		}
 	});
 
